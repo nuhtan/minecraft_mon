3467e5283cfb982ace30f6c9c21362d24c0ef597
nuhtan <ncrichardson15@gmail.com>
2021-01-02 19:22:47 -0500

    added state enums for stracking mc and web

--- src/functions/shared_data.rs
+++ src/functions/shared_data.rs
@@ -10,6 +10,8 @@ pub struct ServerSharedData {
     pub current_player_count: Arc<Mutex<u32>>,
     pub current_players: Arc<Mutex<Vec<String>>>,
     pub max_player_count: Arc<Mutex<u32>>,
+    pub mcserver_state: Arc<Mutex<MinecraftServerState>>,
+    pub gen_state: Arc<Mutex<GeneralState>>
 }
 
 impl ServerSharedData {
@@ -19,6 +21,22 @@ impl ServerSharedData {
             current_player_count: Arc::new(Mutex::new(0)),
             current_players: Arc::new(Mutex::new(Vec::<String>::new())),
             max_player_count: Arc::new(Mutex::new(0)),
+            mcserver_state: Arc::new(Mutex::new(MinecraftServerState::Starting)),
+            gen_state: Arc::new(Mutex::new(GeneralState::Running))
         }
     }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum MinecraftServerState {
+    Off,
+    Starting,
+    Running,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum GeneralState {
+    Running,
+    ShutDown,
+    Restart,
 }
